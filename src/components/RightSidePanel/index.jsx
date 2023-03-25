fc8ecf8acbdf878e338b7d275a4e305fbb92cf6b
mohammad-moa <aahrari5@gmail.com>
2023-03-25 12:52:08 +0430

    added home page and right and left side panels components

---
+++ src/components/RightSidePanel/index.jsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <div>
+            right side panel
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import LeftSidePanel from "../components/LeftSidePanel"
+import RightSidePanel from "../components/RightSidePanel"
+
+export default () => {
+    return (
+        <>
+            <LeftSidePanel />
+            <RightSidePanel />
+        </>
+    )
+}
