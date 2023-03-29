2e64d73eff2e3d6ed4e2fa222fcdb9dc7170f756
mohammad-moa <aahrari5@gmail.com>
2023-03-29 13:56:22 +0430

    updated map reusable and common component
    Signed-off-by: mohammad-moa <aahrari5@gmail.com>

--- src/components/Common/Map.jsx
+++ src/components/Common/Map.jsx
@@ -1,21 +1,31 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { useEffect } from "react";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 
 import 'leaflet/dist/leaflet.css'
 
-export default ({ position, iconUrl, iconSize, zoom }) => {
+export default ({ position, iconUrl, iconSize, zoom, ...props }) => {
 
     const customPinIcon = new L.Icon({
         iconUrl: iconUrl,
         iconSize: iconSize,
     })
 
+    const ChangeCenterClient = () => {
+        const chnageMap = useMap();
+        useEffect(() => {
+            chnageMap.setView(position);
+        }, [position]);
+        return null;
+    }
+
     return (
         <>
-            <MapContainer className="w-full h-full z-0 2xl:h-screen" center={position} zoom={zoom} scrollWheelZoom={false}>
+            <MapContainer {...props} center={position} zoom={zoom} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <ChangeCenterClient />
                 <Marker
                     position={position}
                     icon={customPinIcon}
